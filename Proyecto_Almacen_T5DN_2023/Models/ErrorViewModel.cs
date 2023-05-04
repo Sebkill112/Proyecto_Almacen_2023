2f31ae0425c175faa1e7287f5047388c91250e56
Sebkill112 <snavarrosam45@gmail.com>
2023-05-04 14:39:19 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_Almacen_T5DN_2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Almacen_T5DN_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
