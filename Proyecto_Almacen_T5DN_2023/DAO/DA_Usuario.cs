15743115e31a2b765793c107b65c33ed2048cb58
Sebkill112 <snavarrosam45@gmail.com>
2023-05-04 14:49:43 -0500

    Principio del login

---
+++ Proyecto_Almacen_T5DN_2023/DAO/DA_Usuario.cs
@@ -0,0 +1,14 @@
+﻿using Proyecto_Almacen_T5DN_2023.Models;
+
+namespace Proyecto_Almacen_T5DN_2023.DAO
+{
+    public class DA_Usuario
+    {
+        IEnumerable<Usuario> ListarUsuarios()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            return lista;
+        }
+    }
+}
---
+++ Proyecto_Almacen_T5DN_2023/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto_Almacen_T5DN_2023.Models
+{
+    public class Usuario
+    {
+        public int idRol { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string? Clave { get; set; }
+        public string? rol { get; set; }
+        
+    }
+}
