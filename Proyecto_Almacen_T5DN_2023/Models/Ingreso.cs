3f93fb73070286c0cfbb8b262192863308885d53
Sebkill112 <snavarrosam45@gmail.com>
2023-06-05 10:43:10 -0500

    Arreglo final

---
+++ Proyecto_Almacen_T5DN_2023/Models/Ingreso.cs
@@ -0,0 +1,13 @@
+﻿namespace Proyecto_Almacen_T5DN_2023.Models
+{
+    public class Ingreso
+    {
+        public int idIngreso { get; set; }
+        public string idProveedor { get; set; }
+        public DateTime fechaIngreso { get; set; }
+        public decimal total { get; set; }
+
+        public List<DetalleIngreso> lstdetalleIngreso { get; set; }
+
+    }
+}
