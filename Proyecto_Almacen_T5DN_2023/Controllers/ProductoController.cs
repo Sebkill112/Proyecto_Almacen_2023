216e2380b6a77019fceb9f863de6a022440a39f9
JulioAlbertoYnca <julioynca7228@gmail.com>
2023-06-25 08:22:15 -0500

    2

--- Proyecto_Almacen_T5DN_2023/Controllers/ProductoController.cs
+++ Proyecto_Almacen_T5DN_2023/Controllers/ProductoController.cs
@@ -1,26 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Proyecto_Almacen_T5DN_2023;
 using Proyecto_Almacen_T5DN_2023.DAO;
 using Proyecto_Almacen_T5DN_2023.Models;
-using System.Data.SqlClient;
 
 namespace Proyecto_Almacen_T5DN_2023.Controllers
 {
     public class ProductoController : Controller
     {
-        DA_Producto dao = new DA_Producto();
-        public IConfiguration Configuration { get;}
+        // cadena de connecion I
+        private readonly string cadena_cn;
         public ProductoController(IConfiguration configuration)
         {
-            Configuration = configuration;
+            cadena_cn = configuration.GetConnectionString("cnDB");
         }
+        // cadena de connecion F
+        public DA_Producto dao = new DA_Producto(); 
+        Producto Search(int id)
+        { 
+            Producto b = dao.ListProducto().Where(p => p.idProducto == id).FirstOrDefault();
+
+        }
+
+
+
+
+
+
+
+
+
         public IActionResult Index()
         {
             return View();
-        }
-        public IActionResult ListProductos()
-        { 
-            return View(dao.ListProducto());
         }
     }
 }
--- Proyecto_Almacen_T5DN_2023/DAO/DA_Producto.cs
+++ Proyecto_Almacen_T5DN_2023/DAO/DA_Producto.cs
@@ -2,38 +2,43 @@
 using System.Data.SqlClient;
 
 
-
 namespace Proyecto_Almacen_T5DN_2023.DAO
 {
     public class DA_Producto
     {
-        public string cnn = "";
+        public string cn = "";
         public DA_Producto() 
         {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appSettings.json").Build();
-            cnn = builder.GetSection("ConnectionStrings:cnDB").Value;
+            var builder = new ConfigurationBuilder().SetBasePath(
+                Directory.GetCurrentDirectory()).AddJsonFile("appSettings.json").Build();
+
+            cn = builder.GetSection("ConnectionStrings:cnDB").Value;
         }
-        public List<CProducto> ListProducto() 
+        public List<Producto> ListProducto()
         {
-            SqlConnection cn = new SqlConnection(cnn);
-            List<CProducto> list = new List<CProducto>();
-            SqlCommand cmd = new SqlCommand("dbo.usp_listProductos", cn);
+            SqlConnection cn = new SqlConnection();
+            List<Producto> list =new List<Producto>();
+            SqlCommand cmd = new SqlCommand("", cn);
             cn.Open();
             SqlDataReader rd = cmd.ExecuteReader();
             while (rd.Read()) 
             {
-                list.Add(new CProducto()
+                list.Add(new Producto
                 {
                     idProducto = rd.GetInt32(0),
                     nombreProducto = rd.GetString(1),
-                    nombreProveedor = rd.GetString(2),
+                    idProveedor = rd.GetString(2),
                     stock = rd.GetInt32(3),
                     precio = rd.GetDecimal(4),
-                    nomCat = rd.GetString(5),
+                    idCat = rd.GetInt32(5)
+
                 });
             }
-            cn.Close(); 
-            return list;
+            cn.Close();
+            return list; 
         }
+
+
+
     }
 }
